82a1f3cbafcf479a9ac8f0ea09fef2421a33244e
Venkatesh9963 <devallavenkatesh3@gmail.com>
2026-02-15 18:33:34 +0530

    Time: 4 ms (52.91%) | Memory: 45.3 MB (58.80%) - LeetSync

---
+++ 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public int[] finalPrices(int[] prices) {
+        int n = prices.length;
+        int[] ans = prices.clone();
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < n; i++) {
+            while (!stack.isEmpty() && prices[stack.peek()] >= prices[i]) {
+                int idx = stack.pop();
+                ans[idx] = prices[idx] - prices[i];
+            }
+            stack.push(i);
+        }
+
+        return ans;
+    }
+}
