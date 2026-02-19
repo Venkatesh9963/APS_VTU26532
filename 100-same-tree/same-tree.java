5b11f7cb10fe57b09c80d6893602c5aa3500af25
Venkatesh9963 <devallavenkatesh3@gmail.com>
2026-02-19 11:21:50 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (86.69%) - LeetSync

--- 100-same-tree/same-tree.java
+++ 100-same-tree/same-tree.java
@@ -1,23 +1,21 @@
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        
-        // If both are null → same
-        if(p == null && q == null) {
+        // If both nodes are null, trees are same
+        if (p == null && q == null) {
             return true;
         }
         
-        // If one is null → not same
-        if(p == null || q == null) {
+        // If one is null and other is not, trees are not same
+        if (p == null || q == null) {
             return false;
         }
         
-        // If values different → not same
-        if(p.val != q.val) {
-            return false;
+        // Check current node value and recursively check left & right
+        if (p.val == q.val) {
+            return isSameTree(p.left, q.left) && 
+                   isSameTree(p.right, q.right);
         }
         
-        // Check left and right subtree
-        return isSameTree(p.left, q.left) && 
-               isSameTree(p.right, q.right);
+        return false;
     }
 }
