543380a16fa2e802dc5206ea4fcaaa474123d6ef
Devalla Venkatesh <devallavenkatesh3@gmail.com>
2026-03-27 09:21:45 +0530

    Time: 23 ms (29.58%) | Memory: 59.7 MB (42.89%) - LeetSync

--- 969-number-of-recent-calls/number-of-recent-calls.java
+++ 969-number-of-recent-calls/number-of-recent-calls.java
@@ -2,19 +2,19 @@ import java.util.*;
 
 class RecentCounter {
 
-    private Queue<Integer> queue;
+    Queue<Integer> queue;
 
     public RecentCounter() {
         queue = new LinkedList<>();
     }
-    
+
     public int ping(int t) {
         queue.offer(t);
-        
+
         while (queue.peek() < t - 3000) {
             queue.poll();
         }
-        
+
         return queue.size();
     }
 }
